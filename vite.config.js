91057b58580dbab75abb598a4968097b5b5d0b60
anhkiet <anhkiet18072002@gmail.com>
2025-08-04 11:36:07 +0700

    update vite file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio', // 👈 THÊM DÒNG NÀY
+  base: '/Portfolio/', 
   plugins: [react()],
 })
